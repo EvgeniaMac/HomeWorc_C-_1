9a3236ac9e1e51f5f535504e420b28370ea30f63
«Evgenia <evgenia.kopitext@gmail.com>
2022-06-25 21:38:04 +1100

    Tasc002

---
+++ Tasc002/Program.cs
@@ -0,0 +1,12 @@
+﻿// задача наопределение четное или нечетное число
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number%2 ==0)
+{
+    Console.WriteLine("Вы ввели четное число");
+}
+else{
+    Console.WriteLine("Вы ввели нечетноее число");
+}
