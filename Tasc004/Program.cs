d153f1388566b0718b2d19a065405239cef50336
«Evgenia <evgenia.kopitext@gmail.com>
2022-06-26 08:20:49 +1100

    ignore

--- Tasc004/Program.cs
+++ Tasc004/Program.cs
@@ -1,25 +1,35 @@
-﻿//Задача на определение максимального и минимального числа из 2-х
+﻿//Задача на определение максимального числа из 3-х
 Console.WriteLine("Введите первое число:");
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число:");
 int number2 = Convert.ToInt32(Console.ReadLine());
-int min = 0;
+Console.WriteLine("Введите третье число:");
+int number3 = Convert.ToInt32(Console.ReadLine());
 int max = 0;
 
-if(number1>number2)
+if(namber1>namber2)
 {
-    max = number1;
-    Console.WriteLine("Максиум:");
-    Console.WriteLine(max);
-    min = number2;
-    Console.WriteLine("Минимум:");
-    Console.WriteLine(min);
+    if(namber1>namber3)
+    {
+        max = number1;
+        Console.WriteLine("Максиум:");
+        Console.WriteLine(max);    
+    }
 }
-else{
-max = number2;
-Console.WriteLine("Максиум:");
-Console.WriteLine(max);
-min = number1;
-Console.WriteLine("Минимум:");
-Console.WriteLine(min);
+else
+if(namber2>namber3)
+{
+    if(namber2>namber1)
+    {
+        max = number2;
+        Console.WriteLine("Максиум:");
+        Console.WriteLine(max);    
+    }
+    else
+    if(namber3>namber1)
+    {
+        max = number3;
+        Console.WriteLine("Максиум:");
+        Console.WriteLine(max);    
+    }
 }
