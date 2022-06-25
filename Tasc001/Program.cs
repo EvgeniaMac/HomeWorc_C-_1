1730efa9a20db1ac03169e1194913ffe4ec9c5c3
«Evgenia <evgenia.kopitext@gmail.com>
2022-06-26 08:07:07 +1100

    Tasc001

---
+++ Tasc001/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача на вывод всех четных чисел от 1 до N
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+while(number>1)
+{
+    if(number%2==0)
+    {
+    Console.WriteLine(number);
+    number = number - 1;
+    }
+    number = number - 1;
+}
