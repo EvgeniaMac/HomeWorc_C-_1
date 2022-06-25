1eb4afdc585d9cb1675e330de8294c086ad86237
«Evgenia <evgenia.kopitext@gmail.com>
2022-06-25 19:28:35 +1100

    clouse

--- Tasc004/Program.cs
+++ Tasc004/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.WriteLine("Введите первое число:");
+﻿//Задача на определение максимального и минимального числа из 2-х
+Console.WriteLine("Введите первое число:");
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число:");
 int number2 = Convert.ToInt32(Console.ReadLine());
